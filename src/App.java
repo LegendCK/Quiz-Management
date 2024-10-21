25b061809496750942b7cc355edf8c952efa1888
LegendCK <chiragkhairnar12@gmail.com>
2024-10-21 09:26:29 +0530

    Added LoginView and SignUpView to main app.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+import views.LoginView;
+import views.SignUpView;
+
 public class App {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Quiz Management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+
+        LoginView loginView = new LoginView(cardLayout, mainPanel);
+        SignUpView signUpView = new SignUpView(cardLayout, mainPanel);
+
+        mainPanel.add(loginView, "loginPanel");
+        mainPanel.add(signUpView, "signUpPanel");
+
+        frame.add(mainPanel);
+        frame.setVisible(true);
     }
+
 }
+
